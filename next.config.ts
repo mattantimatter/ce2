43759d7c1dc194f6ec5a336489d76d1f11562129
Matthew Bravo <matt@antimatterai.com>
2025-11-27 01:58:18 -0500

    Suppress DEP0169 url.parse deprecation warning from dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
 import type { NextConfig } from "next";
 
+// Suppress the url.parse deprecation warning from dependencies
+if (typeof process !== 'undefined') {
+  const originalEmit = process.emit;
+  // @ts-expect-error - process.emit has complex typing
+  process.emit = function (name, data, ...args) {
+    if (
+      name === 'warning' &&
+      typeof data === 'object' &&
+      data.name === 'DeprecationWarning' &&
+      data.code === 'DEP0169'
+    ) {
+      return false;
+    }
+    return originalEmit.apply(process, [name, data, ...args]);
+  };
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
